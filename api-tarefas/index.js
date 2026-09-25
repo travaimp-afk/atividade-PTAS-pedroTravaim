2f066c50f4a73c89b0067d3c01d2e8a15615cda2
travaimp-afk <travaimp@gmail.com>
2026-09-25 03:05:35 +0000

    edit: quinto commit, adicionei o expressjson e criei a rota post para tarefas, agora falta o 6, que é testar todas as totas criadas

--- api-tarefas/index.js
+++ api-tarefas/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
 res.send('api de tarefas rodando');});
@@ -14,7 +15,6 @@ const { concluida } = req.query;
 
 if (concluida) {
 return res.json(tarefas.filter(t => String(t.concluida) === concluida));}
-
 res.json(tarefas);});
 
 app.get('/tarefas/:id', (req, res) => {
@@ -22,8 +22,16 @@ const tarefa = tarefas.find(t => t.id === Number(req.params.id));
 
 if (!tarefa) {
 return res.status(404).json({erro: 'sem tarefa'});}
-
 res.json(tarefa);});
+
+app.post('/tarefas', (req, res) => {
+const { titulo } = req.body;
+
+const novaTarefa = {
+id: tarefas.length + 1, titulo, concluida: false};
+
+tarefas.push(novaTarefa);
+res.status(201).json(novaTarefa);});
 
 app.listen(3000, () => {
 console.log('roda na 3000');});
