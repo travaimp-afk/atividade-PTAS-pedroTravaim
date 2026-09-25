88e9e68fb5a75ec31a23569409284cdb559f20a9
travaimp-afk <travaimp@gmail.com>
2026-09-25 00:22:09 +0000

    edit: criei a rota get de tarefas para retornar um array com 3 acoes

--- api-tarefas/index.js
+++ api-tarefas/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-res.send('api de tarefas no ar');});
+res.send('api de tarefas rodando');});
+
+let tarefas = [
+{id:1, titulo: 'estudar',concluida: false },
+{id:2, titulo: 'correr',concluida: true },
+{id:3, titulo: 'almoçar',concluida: false }];
+
+app.get('/tarefas', (req, res) => {
+  res.json(tarefas)});
 
 app.listen(3000, () => {
-console.log('rodando na 3000');});
+  console.log('roda na 3000');});
